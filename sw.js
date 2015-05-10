0b3be7cf5c8cf2a6a8a88320e6585cc46a6b9d41
Naman Nehra <naman.nehra98@gmail.com>
2015-05-10 23:59:52 +0530

    0.2.2

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
-// 0.2.1
+// 0.2.2
 importScripts('assets/cache-polyfill.js');
 var toCache = [
 	'.',
-	'tic-tac-toe/tic-tac-toe.html'
+	'tic-tac-toe/tic-tac-toe.html',
+	'icons/icon-1x.png',
+	'icons/icon-2x.png',
+	'icons/icon-3x.png',
+	'icons/icon-4x.png'
 ];
 self.addEventListener('install', function(e) {
 	e.waitUntil(caches.open('cache').then(function(cache) {
